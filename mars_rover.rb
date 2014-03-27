1293c67b71f5c3dc60821f675f033b040f8ebe8e
Jonathan Chang <jonoschang7@gmail.com>
2014-03-26 21:01:33 -0400

    Refactordirection method to have less code

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,40 +1,17 @@
 # Determines which direction robot is facing
+
+
 def direction(lr, face)
-	if face == "N"
-		if lr == "L"
-			return "W"
-		elsif lr == "R"
-			return "E"
-		else
-			return "LR ERROR, DONT KNOW INPUT"
-		end
-	elsif face == "E"
-		if lr == "L"
-			return "N"
-		elsif lr == "R"
-			return "S"
-		else
-			return "LR ERROR, DONT KNOW INPUT"
-		end
-	elsif face == "S"
-		if lr == "L"
-			return "E"
-		elsif lr == "R"
-			return "W"
-		else
-			return "LR ERROR, DONT KNOW INPUT"
-		end
-	elsif face == "W"
-		if lr == "L"
-			return "N"
-		elsif lr == "R"
-			return "S"
-		else
-			return "LR ERROR, DONT KNOW INPUT"
-		end
-	else
-		return "ERROR DONT KNOW YOUR INPUT"
-	end
+  turn = {"L" => -1, "R" => 1}
+  dir = ["N","E" ,"S" ,"W"]
+
+  if lr == "L" && face == "N"
+  	return "W"
+  elsif lr == "R" && face == "W"
+  	return "N"
+  else
+  	return dir[dir.index(face)+turn[lr]]
+  end  
 end
 
 # movement - if direction is N or S then add/subtract from y coordinate
@@ -55,3 +32,4 @@ def moving(direction)
 	end
 end
 
+puts direction("L", "E")
