6099f6c104f8d8aa7bd3ed69d907b8dd6498fb81
Jonathan Chang <jonoschang7@gmail.com>
2014-03-26 21:24:33 -0400

    Complete command_split method and grid method

--- mars_rover.rb
+++ mars_rover.rb
@@ -3,11 +3,13 @@ def direction(lr, face)
   turn = {"L" => -1, "R" => 1}
   dir = ["N","E" ,"S" ,"W"]
 
+  # Checks to see if condition met to go past index
   if lr == "L" && face == "N"
   	return "W"
   elsif lr == "R" && face == "W"
   	return "N"
   else
+  	# Navigate the dir array based on inputs
   	return dir[dir.index(face)+turn[lr]]
   end  
 end
@@ -23,7 +25,23 @@ def moving(direction)
 end
 
 # Split user commands into single letter commands
-def user_command(command)
+def command_split(command)
   return command.split('')
+end
+
+# Gets user commands
+def user_command
+	puts "Please enter commands on one line (example - LMLMLMLMM )"
+	return gets.chomp
+end
+
+# Defines max grid from user input
+def grid
+	puts "Enter a max size for x co-ordinate"
+	x = gets.chomp.to_i
+	puts "Enter a max size for y co-ordinate"
+	y = gets.chomp.to_i
+  puts "Your grid is [#{x},#{y}]"
+	return [x, y]
 end
 
