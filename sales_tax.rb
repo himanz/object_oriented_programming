9af618ca5ba268eb9220d66ccf956a06bf4df269
Jonathan Chang <jonoschang7@gmail.com>
2014-03-27 14:17:47 -0400

    Finish tax_calc method

--- sales_tax.rb
+++ sales_tax.rb
@@ -3,6 +3,7 @@ class Taxes
   	@items = items
   end
 
+  # Split the sentence into an array of words
   def sentence_split(sentence)
   	arr = []
   	sentence.each do |item|
@@ -11,6 +12,7 @@ class Taxes
   	@items = arr
   end
   
+  # Remove the at word as it is useless
   def remove_at(item_arr)
     item_arr.each {|x| x.delete("at")}
     return item_arr
@@ -19,18 +21,60 @@ class Taxes
   def go
   	sentence_split(@items)
   	remove_at(@items)
+  	tax_calc
   	joining(@items)
   	print @items
+  	
   end
-
+  
+  # Join the array together to create the product 
   def joining(item_arr)
   	arr2 = []
   	item_arr.each { |x| arr2 << x.join(" ")}
   	@items = arr2
   end
+  
+  # Calculate the tax rate
+  def tax_calc
+  	 
+  	basic = ["book", "chocolate", "chocolates", "pills"]
+  	# Go through each sentence of word to find specific words to determine tax
+  	@items.each do |sentence|
+  		# To see if it is exempt from basic tax or not
+  		basic_tax = true
+  		tax = 0
+  		
+  		# Looks for imported for luxary tax
+  		if sentence.include?("imported")
+  			
+  			tax += 5
+  		end
+      
+      # Go through each keyword exempt from basic tax
+      basic.each do |x|      	
+  	    if sentence.include? x
+  	    	basic_tax = false
+  	    	break    
+  	    end
+  		end
+
+  		if basic_tax == true
+  			tax += 10
+  		end
+
+      # Append the tax rates to each sentence 
+      sentence << tax
+  	end
+
+  	
+  end
 end
 
 input1 = Taxes.new(["1 book at 12.49", "1 music CD at 14.99", 
 	                  "1 chocolate bar at 0.85"])
+input2 = Taxes.new(["1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99",
+                    "1 packet of headache pills at 9.75", 
+                    "1 box of imported chocolates at 11.25"])
 
 input1.go
+input2.go
