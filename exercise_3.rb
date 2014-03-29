963b077aca09957891a98d11bef2ed102b5a23cf
Jonathan Chang <jonoschang7@gmail.com>
2014-03-29 10:54:59 -0400

    Create class Tax, Manipulation, Command, work on run

--- exercise_3.rb
+++ exercise_3.rb
@@ -1,52 +1,27 @@
-# sentence.split to get each word seperately in an array
+class Tax
+end
 
-# in a method use .include? books, food, medical products to see
-# if except from tax
+class Manipulation
+end
 
-# do the same for imported goods .include? for additional sales tax
-
-# delete at from the array to get it set up to create a hash
-# start of array to -2 will be the key
-# array -1 will be the value (need to add sales tax to this) 
-
-class Taxes
-  
-  def initialize(items)
-  	@items = items
+class Command
+  def initialize
+    @receipt = []
   end
-
-  def sentence_split(item_arr)
-  	arr = []
-  	item_arr.each do |item|
-      arr << item.split(" ")
-  	end
-  	return arr
-  end
-
-  def remove_at(item_arr)
-    item_arr.each {|x| x.delete("at")}
-    return item_arr
-  end
-  
-  def tax_calc(item_arr)
-  	tax = 0
-  end
-
-  def go
-    arr2 = []
-    arr = @items
-    arr = self.remove_at(self.sentence_split)
-    sentence_split(@items)
-    remove_at(@items)
-    puts "\n" 
-    arr.each { |x| arr2 << x.join(" ") }
-    
-    
-    
-    print arr2 
+  def run
+    done = false
+    while !done
+      puts "Please enter a line(type EXIT when you are done)"
+      input = gets.chomp
+      if input == "EXIT"
+        done = true
+      else
+        @receipt << input
+      end
+    end
+    print @receipt
   end
 end
 
-input1 = Taxes.new(["1 book at 12.49", "1 music CD at 14.99", 
-	                  "1 chocolate bar at 0.85"])
-input1.go
+jon = Command.new
+jon.run
