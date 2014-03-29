2f90d960b280e7aaff811d41cedc774785857016
Jonathan Chang <jonoschang7@gmail.com>
2014-03-29 14:34:23 -0400

    Finish basic and luxury

--- exercise_3.rb
+++ exercise_3.rb
@@ -20,6 +20,25 @@ class Manipulation
     input.delete_if {|x| x == "at" }
     
   end
+  
+  def luxury(input)
+    if input.include? "imported"
+      return 5
+    else 
+      return 0
+    end
+  end
+
+  def basic(input)
+    tax = 10
+    exempt = ["chocolate", "chocolates", "pills", "book"]
+    exempt.each do |x|
+      if input.include? x
+        tax = 0
+      end
+    end
+    return tax
+  end
 
 end
 
@@ -37,10 +56,22 @@ class Command
       else
         #@receipt << input
         @manip = Manipulation.new(input)
-        split_sentence = @manip.split_to_single
-        at_removed = @manip.remove_at(split_sentence)
-        #whole_sentence = @manip.join_words(at_removed)
         
+        # Setting up values to use do manipulation
+        split_sentence = @manip.split_to_single
+        price = split_sentence[-1].to_f
+        at_removed = @manip.remove_at(split_sentence)
+    
+        luxury_rate = @manip.luxury(at_removed)
+        basic_rate = @manip.basic(at_removed)
+        puts "-------------"
+        puts "luxury tax is #{luxury_rate}"
+        puts "--------------"
+        puts "basic tax is #{basic_rate}"
+        puts "--------------"
+        #whole_sentence = @manip.join_words(at_removed)
+        puts price
+        puts "\n"
         print at_removed
       end
     end
