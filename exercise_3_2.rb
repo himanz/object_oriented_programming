4f02bc1d865346f399e04b09a8e2afa93368783d
Jonathan Chang <jonoschang7@gmail.com>
2014-03-31 19:12:23 -0400

    Work on run

--- exercise_3_2.rb
+++ exercise_3_2.rb
@@ -1,24 +1,29 @@
 class Price
 end
 
+class Manipulation
+end	
+
 class User
 	def run
 		@receipt = []
 		done = false
-		while != done
+		while !done
 			puts "Enter a sentence (type EXIT to finish)"
 			input = gets.chomp
-			if input == EXIT
-				break
+			if input == "EXIT"
+				done = true
 			else
 				@receipt << input
 			end
-		end 
+		end
+		print @receipt 
 	end
+
 
 	def user_input
 	end
 end
 
-class Manipulation
-end	
+jon = User.new
+jon.run
