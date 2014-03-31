dfee27e7a61d098500a56f5ffa3486dab8f53ace
Jonathan Chang <jonoschang7@gmail.com>
2014-03-30 22:41:12 -0400

    Finish start

--- mars_rover4.rb
+++ mars_rover4.rb
@@ -1,4 +1,10 @@
 class Rover
+  def initialize(x, y, d)
+  	@x = x
+  	@y = y
+  	@d = d
+  end
+
 	def move
 	end
 
@@ -13,7 +19,18 @@ class Rover
 end
 
 class Commands
+	X = 0
+	Y = 1
+	D = 2
+	def start
+		puts "Enter coordinates and direction like so: 1 3 N"
+		coords = gets.chomp.split(" ")
+		@commands = Rover.new(coords[X].to_i, coords[Y].to_i, coords[D]) 
+	end
 end
 
 class Grid
 end
+
+jon = Commands.new
+jon.start
