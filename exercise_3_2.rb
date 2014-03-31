95be88c105faae1f5ceeb7c3a3ec645a25fd4d51
Jonathan Chang <jonoschang7@gmail.com>
2014-03-31 19:15:15 -0400

    Finish user_input

--- exercise_3_2.rb
+++ exercise_3_2.rb
@@ -7,21 +7,23 @@ end
 class User
 	def run
 		@receipt = []
-		done = false
-		while !done
-			puts "Enter a sentence (type EXIT to finish)"
-			input = gets.chomp
-			if input == "EXIT"
-				done = true
-			else
-				@receipt << input
-			end
+		@done = false
+		while !@done
+			user_input
 		end
 		print @receipt 
 	end
 
 
 	def user_input
+		puts "Enter a sentence (type EXIT to finish)"
+			input = gets.chomp
+			if input == "EXIT"
+				@done = true
+
+			else
+				@receipt << input
+			end
 	end
 end
 
